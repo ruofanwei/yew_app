5087f858b08b8a6f603c067ac4920e5b3a983ac3
ruofan <px732amgo81sp8@gmail.com>
2024-08-11 21:11:53 +0800

    init project

---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+struct Model {
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+enum Msg {
+    AddOne,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            value: 0,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => self.value += 1
+        }
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // 如果有新的不同屬性，應該只能回傳 true
+        // 若是這個元件沒有任何屬性，那就可以只回傳 false
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <button onclick={self.link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<Model>::new().mount_to_body();
+}
